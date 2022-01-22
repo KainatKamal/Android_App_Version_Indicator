68bd7d38867dda25db526bac9676e6789beb3189
Kainat <49153415+KainatKamal@users.noreply.github.com>
2022-01-22 19:24:16 +0530

    Update Android_App_Version_Indicator.py

--- Android_App_Version_Indicator.py
+++ Android_App_Version_Indicator.py
@@ -9,7 +9,6 @@ from os.path import join
 
 
 
-
 def execute_cmd(cmd,text):
 	text=text.upper()
 	p1 = subprocess.Popen(cmd, shell=True, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -32,19 +31,18 @@ execute_cmd("rmdir output /S /Q","dummy")
 execute_cmd("mkdir output","dummy")
 execute_cmd("type nul > output.txt","dummy")
  
-
+# Create the excel report
 workbook = xlsxwriter.Workbook('Apps_Version_Indicator.xlsx')
 worksheet = workbook.add_worksheet("Apps_Version_Indicator")
 border = workbook.add_format({'border':1})
 cell_format = workbook.add_format({'bold':True, 'border':2})
 cell_format.set_bg_color('cyan')
- 
-
 worksheet.write('A1', 'Sl. No.', cell_format)
 worksheet.write('B1', 'APK Name', cell_format)
 worksheet.write('C1', 'Package Name', cell_format)
 worksheet.write('D1', 'Version Code', cell_format)
 worksheet.write('E1', 'Version Name', cell_format)
+worksheet.write('F1', 'Min SDK Version', cell_format)
 
 
 apk_path=input("\n Enter the path of the folder where APKs are present: ")
@@ -60,10 +58,8 @@ excel_output_path = current_working_directory + "\\output\\Apps_Version_Indicato
 output_txt_path = current_working_directory + "\\output.txt"
 total_apps = 'dir /a:-d /s /b '+apk_path+' | find /c ":"'
 aapt = aapt_path + " d badging "
-find_version = ' | findstr /i /c:"versionCode=" | findstr /i /r "[0-9]" > output.txt'
+find_version = ' | findstr /i /c:"version" | findstr /i /r "[0-9]" > output.txt'
 delete_output_txt = 'del '+output_txt_path
-
-
 no_of_apps = int(execute_cmd(total_apps,"dummy"))
 print("\n Total no. of apps: ",no_of_apps)
 all_apps = os.listdir(apk_path)
@@ -77,12 +73,29 @@ for apk_name in all_apps:
 	file_content = file.read()
 	package_name=re.search(r"name='(.*?)' versionCode",file_content).group(1)
 	version_code=re.search(r"versionCode='([\d.]+)",file_content).group(1)
+	found1 = bool(version_code)
 	version_name=re.search(r"versionName='([\d.]+)",file_content).group(1)
+	found2 = bool(version_name)
+	try:
+		min_sdk_version=re.search(r"sdkVersion:'([\d.]+)",file_content).group(1)
+	except AttributeError:
+		min_sdk_version=re.search(r"sdkVersion:'([\d.]+)",file_content)
+	found3 = bool(min_sdk_version)
 	worksheet.write(row, column, serial_number, border)
 	worksheet.write(row, column + 1, apk_name, border)
 	worksheet.write(row, column + 2, package_name, border)
-	worksheet.write(row, column + 3, version_code, border)
-	worksheet.write(row, column + 4, version_name, border)
+	if(found1 == True):
+		worksheet.write(row, column + 3, version_code, border)
+	else:
+		worksheet.write(row, column + 3, 'NOT FOUND', border)
+	if(found2 == True):
+		worksheet.write(row, column + 4, version_name, border)
+	else:
+		worksheet.write(row, column + 4, 'NOT FOUND', border)
+	if(found3 == True):
+		worksheet.write(row, column + 5, min_sdk_version, border)
+	else:
+		worksheet.write(row, column + 5, 'NOT FOUND', border)
 	serial_number +=1
 	row +=1
 
@@ -90,7 +103,7 @@ file.close()
 
 
 worksheet.set_column('B:C',50)
-worksheet.set_column('D:E',25)
+worksheet.set_column('D:F',25)
 workbook.close()
 
 
